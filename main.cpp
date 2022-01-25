904305b5df226d5152aa1977bdddacbe9f1a5303
Meski12 <98364457+Meski12@users.noreply.github.com>
2022-01-24 16:32:55 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,13 @@
 //main.cpp
+#include <iostream> 
+using namespace std; 
+int hello1Function(string name1); 
+int hello2Function(string name2); 
+int main() 
+{ 
+ string name1="your name1"; 
+ hello1Function(name1); 
+ string name2="Jordan Martinez"; 
+ hello2Function(name2); 
+ return 0; 
+} 
