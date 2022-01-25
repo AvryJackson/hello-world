6c40c0f1fb095b1f52e6f17c82cb3480c5a241db
AvryJackson <45529827+AvryJackson@users.noreply.github.com>
2022-01-24 16:34:32 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ int hello1Function(string name1);
 int hello2Function(string name2); 
 int main() 
 { 
- string name1="your name1"; 
+ string name1="Avry Jackson"; 
  hello1Function(name1); 
  string name2="Jordan Martinez"; 
  hello2Function(name2); 
